6f1f0ae9e14e593c65dbf1dd38f5f43edb588374
Daniel Protopopov <dprpg@yahoo.com>
2021-03-13 00:30:23 +0300

    Added composer livewire directive, and rolled back welcome.blade.php

--- resources/views/providers.blade.php
+++ resources/views/providers.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Exchanges') }}
+            {{ __('Providers') }}
         </h2>
     </x-slot>
 
